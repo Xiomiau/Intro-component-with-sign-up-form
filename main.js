86f79addd59f0ac6919a763dbb034b10df9a9e61
Xiomara Arzola <xiomi_arzola@outlook.com>
2022-08-02 09:32:16 -0700

    email validation ok

--- main.js
+++ main.js
@@ -12,23 +12,37 @@ const sumbit = document.querySelector('#button');
 
 sumbit.addEventListener('click', (e)=> {
     e.preventDefault()
-    validateEmpty(firstName.value, firstName);
+    validateEmpty(firstName.value, firstName,firstNameError, 'First Name canot be empty');
+    validateEmpty(lastName.value, lastName, LastNameError, 'Last Name canot be empty');
+    validateEmail(email.value, email, emailError);
+    validateEmpty(password.value, password, passwordError,'Password');
 });
 
-function validateEmpty (valueInput, divError) {
-    if(valueInput==0) {
-        showError(divError)
+function validateEmail(valueInput, divInput,divError) {
+    let regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    if(regEx.test(valueInput)==true){
+        hideError(divInput,divError)
     } else {
-        hideError()
+        showError(divInput,divError, 'Looks like this is not an email')
+
     }
 }
 
-function showError (divError) {
-    divError.style.border='1px solid red';
+function validateEmpty (valueInput,divInput,divError,nameInput) {
+    if(valueInput.length==0) {
+        showError(divInput,divError,nameInput)
+    } else {
+        hideError(divInput,divError)
+    }
 }
+function showError (divInput,divError,error) {
+    divInput.style.border='1px solid red';
+    divError.innerHTML = `<img id="icon-error" class="error-icon" src="/images/icon-error.svg" alt="icon error"> <p id="nameError" class="text-error"> ${error} </p>`;
+} 
 
-function hideError(divError) {
-
+function hideError(divInput,divError) {
+    divInput.style.border='1px solid hsl(246, 25%, 77%)';
+    divError.innerHTML='';
 }
 
 
